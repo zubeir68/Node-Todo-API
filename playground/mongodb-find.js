92048c1605c23de0de6bddd8b3ce05da1769a131
zubeir68 <zubeir.mohamed@outlook.de>
2019-01-20 02:51:01 +0100

    New todo api using nodejs

---
+++ playground/mongodb-find.js
@@ -0,0 +1,41 @@
+//const {MongoClient, ObjectID} = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (err, client) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connect to MongoDB server");
+    const db = client.db('TodoApp');
+
+    // db.collection("Todos")
+    //   .find({
+    //     _id: new ObjectID("")
+    //       })
+    //   .toArray()
+    //   .then(
+    //     docs => {
+    //       console.log("Todos");
+    //       console.log(JSON.stringify(docs, undefined, 2));
+    //     },
+    //     err => {
+    //       console.log("Unable to fetch todos", err);
+    //     }
+    //   );
+
+    db.collection("Todos")
+      .count()
+      .then(
+        count => {
+          console.log(`Count: ${count}`);
+        },
+        err => {
+          console.log("Unable to fetch todos", err);
+        }
+      );
+
+    client.close();
+  }
+);
