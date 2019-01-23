33e8ae3ddbf0293cd9bce2e2f1d2c20b49d26922
zubeir68 <zubeir.mohamed@outlook.de>
2019-01-23 06:22:37 +0100

    fixed Delete request for todos/:id

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.get("/todos/:id", (req, res) => {
 });
 
 //DELETE /todo/:id
-app.delete('todos/:id', (req,res) => {
+app.delete('/todos/:id', (req,res) => {
   let id = req.params.id;
 
   if(!ObjectID.isValid(id)) {
@@ -83,7 +83,7 @@ app.delete('todos/:id', (req,res) => {
     if(!todo) {
      return res.status(404).send();
     }
-   res.status(200).send();
+   res.send(todo);
   }, e => res.status(400).send());
 });
 
