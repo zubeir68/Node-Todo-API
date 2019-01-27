8e46ebb8a7cf835b0655e943f8176cdb7b798501
zubeir68 <zubeir.mohamed@outlook.de>
2019-01-27 02:10:01 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb"); //check
@@ -88,6 +89,36 @@ app.delete('/todos/:id', (req,res) => {
 });
 
 
+//PATCH /todos/:id
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  //with lodash this will specify only what you can edit
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch(e => {
+    res.status(400).send()
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Started on port: ${port}`);
