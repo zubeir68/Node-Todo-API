ff6f74a1c8cf4d5a2eb167bc336ca00179bd046b
zubeir68 <zubeir.mohamed@outlook.de>
2019-02-05 03:50:30 +0100

    Refactoring test files for testing user

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,27 +3,11 @@ const request = require("supertest");
 const { ObjectID } = require("mongodb");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: "First test todo"
-  },
-  {
-    _id: new ObjectID(),
-    text: "Second test todo",
-    completed: true,
-    completedAt: 333
-  }
-];
 
-beforeEach(done => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos);
-    })
-    .then(() => done());
-});
+beforeEach(populateTodos);
+beforeEach(populateUsers);
 
 describe("POST /todos", () => {
   it("should create a new todo", done => {
