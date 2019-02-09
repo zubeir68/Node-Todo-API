8e10d1523b2cb80997722ef9281810e1051ac1bc
zubeir68 <zubeir.mohamed@outlook.de>
2019-02-09 03:04:11 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -141,6 +141,20 @@ app.post("/users", (req, res) => {
   })
 })
 
+//POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password'])
+  
+  User.findByCredentials(body.email, body.password).then(user => {
+    return user.generateAuthToken().then(token => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch(e => { 
+    res.status(400).send();
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Started on port: ${port}`);
